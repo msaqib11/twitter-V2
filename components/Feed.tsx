5897b94f1e2921c1da1c1e51978b280fb4e3cf04
msaqib11 <saqib.maverick@gmail.com>
2023-04-15 17:30:33 +0500

    react hot toast implemented

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,27 +1,44 @@
 import { Tweet } from "@/typing";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import TweetBox from "./TweetBox";
-import TweetComponent from '../components/Tweet'
+import TweetComponent from "../components/Tweet";
+import { useState } from "react";
+import { fetchTweets } from "@/utils/fetchTweets";
+import { toast } from "react-hot-toast";
 
-interface props  {
-  tweets : Tweet[]
+interface Props {
+  tweets: Tweet[];
 }
 
-const Feed = ({tweets}:props) => {
+const Feed = ({ tweets: tweetsProp }: Props) => {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+  console.log(tweets);
+  
+  const handleRefresh = async () => {
+    const  refreshToast = toast.loading('loading...');
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+    toast.success('Feed Updated!',{
+      id:refreshToast
+    })
+    };
   return (
     <div className="col-span-7 lg:col-span-5 border-x overflow-y-scroll	max-h-screen scrollbar-hide ">
       <div className="flex items-center justify-between ">
         <h1 className="p-5 pb-0 font-bold text-xl ">Home</h1>
-        <ArrowPathIcon className="h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125 mt-3 mr-4" />
+        <ArrowPathIcon
+          onClick={handleRefresh}
+          className="h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125 mt-3 mr-4"
+        />
       </div>
 
       <div>
-        <TweetBox/>
+        <TweetBox />
       </div>
 
       <div>
-        {tweets.map((tweet)=>(
-          <TweetComponent key={tweet._id} tweet= {tweet} />
+        {tweets.map((tweet) => (
+          <TweetComponent key={tweet._id} tweet={tweet} />
         ))}
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,20 @@ import { Tweet } from "@/typing";
 import { fetchTweets } from "@/utils/fetchTweets";
 import { GetServerSideProps } from "next";
 import  Head  from "next/head";
+import { Toaster } from "react-hot-toast";
 
 interface Props {
   tweets : Tweet[]
 }
 
 export default function Home({tweets}:Props) {
-  return <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
+  return <div className="lg:max-w-7xl mx-auto max-h-screen overflow-hidden">
     <Head>
       <title>
           Twitter 2.0
       </title>
     </Head>
+    <Toaster/>
     <main className="grid grid-cols-9">
       <Sidebar/>
       <Feed tweets={tweets}/>
